4f133543d841f9c69461660f3a7527f1de90cb18
z-for-zarrin <zarrin_r01@hotmail.co.uk>
2024-09-03 15:39:34 +0100

    initial commit

---
+++ src/main/java/com/example/stardew_cookbook/StardewCookbookApplication.java
@@ -0,0 +1,13 @@
+package com.example.stardew_cookbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StardewCookbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StardewCookbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/stardew_cookbook/StardewCookbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stardew_cookbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StardewCookbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
